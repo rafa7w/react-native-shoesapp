108d51b84d9fc20b720342c83b9e4f4f559238aa
Rafael William <rafael.fim@hotmail.com>
2023-05-23 21:51:58 -0300

    send data in notification

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -34,7 +34,7 @@ export function Routes() {
       {
         notification?.title &&
         <Notification 
-          title={notification.title}
+          data={notification}
           onClose={() => {
             setNotification(undefined)
           }}
